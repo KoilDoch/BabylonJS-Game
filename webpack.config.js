91f3060d7306e4aba02d3c69fd65f35c9fe31730
kd41 <kd41@hw.ac.uk>
2022-01-19 13:42:57 +0000

    full set up complete, packages and such

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const fs = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const appDirectory = fs.realpathSync(process.cwd());
+
+module.exports = {
+    entry: path.resolve(appDirectory, "src/app.ts"),    //path to main .ts file
+    output: {
+        filename: "js/bundleName.js",    //name for the js file that is created/compiled in memory
+        clean: true,
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    devServer: {
+        host: "0.0.0.0",
+        post: 8080, // port for local host
+        static: path.resolve(appDirectory, "public"), // serve from public folder
+        hot: true,
+        devMiddleware: {
+            publicPath: "/"
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: path.resolve(appDirectory, "public/index.html"),
+        })
+    ],
+    mode: "development",
+};
